dbd6662b5566427379632d8103babe5658c3ed37
Sérgio Pereira Dias Júnior <99135090+Sergiopdias@users.noreply.github.com>
2022-05-16 23:04:19 -0300

    Implementação (sem estilização) da página MEU PERFIL, e setup do globalState

--- src/global/GlobalState.js
+++ src/global/GlobalState.js
@@ -1,10 +1,55 @@
 import React, { useState, useEffect } from 'react'
 import { GlobalContext } from './GlobalContext'
 import useRequestData from '../hooks/useRequestData'
+import { BASE_URL, headers } from "../constants/urls"
+import axios from 'axios'
 
 export default function GlobalState(props) {
-    return(
-        <GlobalContext.Provider>
+
+    //-- Estados & Setters --//
+    const [profile, setProfile] = useState()
+    const [orders, setOrders] = useState()
+    // const [update, setUpdate] = useState(0)
+    // const [address, setAddress] = useState()
+
+    //-- requests --//
+    // const getFullAddress = () => {
+    //     axios
+    //         .get(`${BASE_URL}profile/address`, headers)
+    //         .then((res) => {
+    //             setAddress(res.data)
+    //         })
+    //         .catch((err) => {
+    //             console.log('Deu ruim: ', err.response.data)
+    //         })
+    // }
+    const getProfile = () => {
+        axios
+            .get(`${BASE_URL}profile`, headers)
+            .then((res) => {
+                setProfile(res.data)
+            })
+            .catch((err) => {
+                console.log('Deu ruim: ', err.response.data)
+            })
+    }
+    const getOrdersHistory = () => {
+        axios
+            .get(`${BASE_URL}orders/history`, headers)
+            .then((res) => {
+                setOrders(res.data)
+            })
+            .catch((err) => {
+                console.log('Deu ruim: ', err.response.data)
+            })
+    }
+
+    const states = { profile, orders }
+    const setters = { setProfile, setOrders }
+    const requests = { getProfile, getOrdersHistory }
+
+    return (
+        <GlobalContext.Provider value={{ states, setters, requests }}>
             {props.children}
         </GlobalContext.Provider>
     )
--- src/pages/profilePage/ProfilePage.js
+++ src/pages/profilePage/ProfilePage.js
@@ -1,9 +1,52 @@
-import React from "react"
+import React, { useContext, useEffect } from "react"
+import { useNavigate } from "react-router-dom";
+import { goToAdressPage } from "../../routes/coordinator"
+import { GlobalContext } from "../../global/GlobalContext";
+import OrderCard from "../../components/orders/OrderCard"
 
-function ProfilePage () {
+// colocar use protected page 
+function ProfilePage() {
+  const navigate = useNavigate()
+  const { states, requests } = useContext(GlobalContext)
+
+  // -- Map dos pedidos --//
+  const mapOrders = states.orders?.orders.map((order) => {
+    return (
+      <OrderCard key={order.restaurantName} order={order} />
+    )
+  })
+
+  //-- useEffect ainda sem atualizar automatico --//
+  useEffect(() => {
+    requests.getProfile()
+    requests.getOrdersHistory()
+  }, [])
+
   return (
     <div>
-      Profile
+      <div>
+        <p>
+          {states.profile?.user.name}
+          <br />
+          {states.profile?.user.email}
+          <br />
+          {states.profile?.user.cpf}
+        </p>
+        <button >
+          editar
+        </button>
+      </div>
+      <div>
+        <p>Endereço cadastrado</p>
+        <p>{states.profile?.user.address}</p>
+        <button onClick={() => goToAdressPage(navigate)}>
+          editar
+        </button>
+      </div>
+      <div>
+        <h3>Histórico de pedidos</h3>
+        {mapOrders}
+      </div>
     </div>
   )
 }
