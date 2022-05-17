c7edf6bf31901a5bf7f3d9089070ae4962b9e05d
Matheus Martinelle Barros <matheusmartinellibarros@gmail.com>
2022-05-16 21:10:38 -0300

    Criação de Endereço

--- src/pages/adressPage/styled.js
+++ src/pages/adressPage/styled.js
@@ -1,1 +1,23 @@
 import styled from "styled-components";
+
+export const DivAdress = styled.div`
+    height: 85vh;
+    margin: 45px 15px;
+
+    p{
+        text-align: center;
+    }
+`
+
+export const FormAdress = styled.form`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 15px;
+    margin: 25px;
+
+    input{
+        padding: 5px;
+    }
+`
