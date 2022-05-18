08bf1df5c0a54f96f225eb07798dd00ab614519d
laura amancio <lauran.amancio@yahoo.com.br>
2022-05-18 14:54:47 -0300

    IGNORAR branch de teste do feed IGNORAR

---
+++ src/constants/categorys.js
@@ -0,0 +1,11 @@
+export const restaurantCategory = [
+    "Árabe",
+    "Asiática",
+    "Hamburguer",
+    "Italiana",
+    "Sorvetes",
+    "Carnes",
+    "Baiana",
+    "Petiscos",
+    "Mexicana"
+]
--- src/pages/feedPage/FeedPage.js
+++ src/pages/feedPage/FeedPage.js
@@ -1,38 +1,52 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import { GlobalContext } from "../../global/GlobalContext";
 import CardRestaurant from "./cardRestaurant/CardRestaurant";
-import useForm from "../../hooks/useForm"
-
+import useForm from "../../hooks/useForm";
+// import {restaurantCategory} from "../../constants/categorys"
 
 function FeedPage() {
   const { states, requests } = useContext(GlobalContext);
-  const {form, onChange} = useForm({ search: ""})
+  const { form, onChange } = useForm({ search: "" });
 
 
-  const getFilteredList = states.restaurants?.restaurants.filter((restaurant) =>{
-    return (
-      restaurant.name.toLowerCase().includes(form.search.toLowerCase())
-    )
-  }).map((filtered) =>{
-    return <CardRestaurant filtered={filtered} key={filtered.id}/>
-  })
+  const getFilteredList = states.restaurants?.restaurants
+    .filter((restaurant) => {
+      return restaurant.name.toLowerCase().includes(form.search.toLowerCase());
+    })
+    .map((filtered) => {
+      return <CardRestaurant filtered={filtered} key={filtered.id} />;
+    });
 
-console.log(getFilteredList)
+  // const filterCategory = (category) => {
+  //   const filter = restaurantCategory.map((categoria) =>{
+  //     return categoria
+  //   })
+  //   if(filter === category){
+  //     return <CardRestaurant/>
+  //   }
+  // }
 
+  // const getFilterCategory = states.restaurants?.restaurants.map((restaurant) =>{
+  //   return (
+  //     <h4 onClick={() => filterCategory(restaurant.category)} key={restaurant.id}>{restaurant.category}</h4>
+  //   )
+  // })
+
+
+  
   useEffect(() => {
     requests.getRestaurants();
   }, []);
 
-
   return (
     <div>
-      <input 
-      placeholder="Buscar Restaurantes"
-      onChange={onChange}
-      value={form.search}
-      name={"search"}
+      <input
+        placeholder="Buscar Restaurantes"
+        onChange={onChange}
+        value={form.search}
+        name={"search"}
       />
-
+      {/* {getFilterCategory} */}
       {getFilteredList}
     </div>
   );
