98ccc8e4c1cd4c42625fea3d181f2652a744fa29
Lis Ribeiro <lisribeiro197@gmail.com>
2022-05-20 20:13:08 -0300

    Básico da página de erro

--- src/pages/errorPage/ErrorPage.js
+++ src/pages/errorPage/ErrorPage.js
@@ -1,10 +1,12 @@
 import React from "react"
+import { Typography } from "@material-ui/core";
+import { ErrorPageContainer } from './styled'
 
-function ErrorPage () {
+function ErrorPage() {
   return (
-    <div>
-      ERRO
-    </div>
+    <ErrorPageContainer>
+      <Typography color={"primary"} variant={"h4"} align={"center"}>Erro 404 - Página Não Encontrada </Typography>
+    </ErrorPageContainer>
   )
 }
 
